871ac02ec1e9287a006553676e1f423e7d0a5a3e
misterLeu <leu.jeremy@gmail.com>
2024-02-04 10:25:33 +0100

    feat(main): improve crop handler

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import argparse
 
 from pathlib import Path
-from typing import Union
 
 from CraftPatternGenerator.CraftPatternGenerator import CraftPatternGenerator
+from CraftPatternGenerator.input import preset_colors
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='image to convert')
@@ -12,8 +12,11 @@ if __name__ == '__main__':
     parser.add_argument('color_chart', type=str, action='store',
                         help='name of the chart to use, should match with one of those in input.py')
 
-    parser.add_argument('--crop-color', dest='crop_color', action='store', type=int, nargs="+",
-                        default=[None], help='The color to use for crop the image if not given no crop will be performed')
+    colors = parser.add_mutually_exclusive_group()
+    colors.add_argument('--crop-color-rgb', dest='crop_color_rgb', action='store', type=list, nargs="+",
+                        help='The color to use for crop the image if not given no crop will be performed')
+    colors.add_argument('--crop-color-name', dest='crop_color_name', action='store', type=str,
+                        help='The color to use for crop the image if not given no crop will be performed')
     parser.add_argument('--crop-tolerance', type=int, dest='crop_tolerance', action='store', default=0,
                         help='The tolerance in color space to match for crop, 0 by default,'
                              ' color space is in lab by default')
@@ -27,15 +30,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
     origin_path = args.origin_path
     color_chart = args.color_chart
-    if len(args.crop_color) != 1 and len(args.crop_color) != 3:
-        raise Exception(f"Crop_color should contain exactly 1 or 3 values, currently: {args.crop_color}")
-    if len(args.crop_color) == 3:
-        crop_color = tuple(args.crop_color)
-    if len(args.crop_color) == 1:
-        crop_color = args.crop_color[0]
+    crop_color = None
+    if getattr(args, "crop_color_rgb", None):
+        if len(args.crop_color_rgb) != 3:
+            raise Exception("crop_color_rgb should be a list of 3 integers")
+        try:
+            crop_color = tuple(int(color) for color in args.crop_color)
+        except ValueError:
+            raise Exception("crop_color_rgb list should only contains integer")
+        if any( 0 > color > 255 for color in crop_color):
+            raise Exception("crop_color_rgb list should only contains integer between 0 and 255")
+    if getattr(args, "crop_color_name", None):
+        if args.crop_color_name not in preset_colors:
+            raise Exception(f'crop_color_name should be one of {", ".join(color for color in preset_colors)}')
+        crop_color = preset_colors[args.crop_color_name]
     crop_tolerance = args.crop_tolerance
     if len(args.target_size) != 1 and len(args.target_size) != 2:
         raise Exception(f"target_size should contain exactly 1 or 2 values, currently: {args.target_size}")
+    target_size = None
     if len(args.target_size) == 2:
         target_size = tuple(args.target_size)
     if len(args.target_size) == 1:
